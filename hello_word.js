f0df97a09ebfe5bd798f52954c347857f6238fc1
Rafael-co <rafael.camilo21@outlook.com>
2022-10-07 16:11:27 -0300

    primeira aula de typescript

---
+++ hello_word.js
@@ -0,0 +1,22 @@
+"use strict";
+let username = "shaolin";
+username = "flavio";
+let pi = 678;
+pi = 22;
+let logado = true;
+logado = 0;
+let rg = 458084141;
+rg = '458084141';
+let generico = [];
+generico = {
+    a: '',
+    b: 8,
+    c: true,
+};
+generico = 'Eu sou genérico';
+generico = 6.89;
+const listagemDeNomes = ['joão', 'carlos', 'manoel'];
+const notas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const valores = ['1', 12, 'casa', 145];
+const arr = [true, true, false, true, false, true, true];
+const lista = [true, 0, 8, 'olá', false]; // = igual const list: Array [any] = []
---
+++ hello_word.ts
@@ -0,0 +1,30 @@
+let username = "shaolin"
+username = "flavio"
+
+let pi: number = 678
+pi = 22 
+
+let logado: boolean = true
+logado = 0
+
+let rg : number | string = 458084141
+rg = '458084141'
+
+let generico: any = []
+generico = {
+    a: '',
+    b: 8,
+    c: true,
+}
+
+generico = 'Eu sou genérico'
+generico = 6.89
+
+const listagemDeNomes: string[] = ['joão','carlos','manoel']
+const notas: number[] = [1,2,3,4,5,6,7,8,9,0]
+
+const valores: Array<string | number> = ['1',12,'casa',145]
+
+const arr: Array<boolean> = [true, true, false, true, false,true,true]
+
+const lista: Array<any> = [true,0,8,'olá',false] // = igual const list: Array [any] = []
